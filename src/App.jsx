97882c3b3e22309429524eb615497cb70ef2361b
suryansh0010 <suryanshawasthi9@gmail.com>
2024-07-23 14:46:38 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);  // Corrected typo here
   return (
     <div>
       <Navbar/>
@@ -13,11 +19,16 @@ const App = () => {
       <div className='container'>
         <Title subTitle='OUR PROGRAM' title='What we Offer'/>
         <Programs/>
-        <About/>
+        <About setPlayState={setPlayState}/>
         <Title subTitle='Gallery' title='Campus Photos'/>
-
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What Students Says'/>
+        <Testimonials/>
+        <Title subTitle='Contact Us' title='Get In Touch'/>
+        <Contact/>
+        <Footer/>
       </div>
-      
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>  // Corrected prop passing here
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-scroll';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,19 +19,22 @@ const Navbar = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
   return (
-    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-      <img src={logo} alt="Logo" className='logo' />
-      <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonial</li>
-        <li>
-          <button className='btn'>Contact Us</button>
-        </li>
+    <nav className={`navbar ${sticky ? 'dark-nav' : ''}`}>
+      <img src={logo} alt="Logo" className="logo" />
+      <ul className={`nav-links ${mobileMenu ? 'show-mobile-menu' : 'hide-mobile-menu'}`}>
+        <li><Link to="hero" smooth={true} offset={0} duration={500} onClick={() => setMobileMenu(false)}>Home</Link></li>
+        <li><Link to="Program" smooth={true} offset={-260} duration={500} onClick={() => setMobileMenu(false)}>Program</Link></li>
+        <li><Link to="about" smooth={true} offset={-150} duration={500} onClick={() => setMobileMenu(false)}>About Us</Link></li>
+        <li><Link to="campus" smooth={true} offset={-260} duration={500} onClick={() => setMobileMenu(false)}>Campus</Link></li>
+        <li><Link to="testimonials" smooth={true} offset={-260} duration={500} onClick={() => setMobileMenu(false)}>Testimonials</Link></li>
+        <li><Link to="contact" smooth={true} offset={-260} duration={500} className="btn" onClick={() => setMobileMenu(false)}>Contact Us</Link></li>
       </ul>
+      <img src={menu_icon} alt="Menu" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 }
